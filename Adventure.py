2201e9cd91f38d3d59f737e7355f744a24326efe
Gianluca Di Paola <gianlucadipaola18@gmail.com>
2024-02-29 10:33:17 +0100

    Adding Adventure game

---
+++ Adventure.py
@@ -0,0 +1,11 @@
+
+name = input("Welcome to this adventure, what is your name? ")
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right.\n Which way would you like to go? ").lower()
+
+if answer=="left":
+
+elif answer=="right":
+
+else:
+    print("Not a valid option! \n")
